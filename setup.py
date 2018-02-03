cb198135d5925fcebbd77fb991ea78f1ae3b9e2d
nathanepstein <_@nepste.in>
2018-02-03 11:39:34 -0500

    v0.0.4

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
   name = "PyDexter",
-  version = "0.0.3",
+  version = "0.0.4",
   author = "Nathan Epstein",
   author_email = "_@nepste.in",
   description = ("Simple plotting for Python. Render charts in the browser with simple Python syntax."),
